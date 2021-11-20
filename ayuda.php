0e7dd0e0280529019cf2231311da2db0433c847d
Ormuz1 <mariano722003@gmail.com>
2021-11-20 03:14:51 -0300

    Añadi informacion

--- ayuda.php
+++ ayuda.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Centro de Ayuda</title>
     <link rel="stylesheet" href="CSS/centro_de_ayuda.css">
-    <?php include("HTML/global_dependencies.html");?>
+    <?php include("HTML/global_dependencies.html"); ?>
     <script src="JS/ayuda.js"> </script>
 </head>
+
 <body>
-    <?php include("HTML/navbar.html");?>
+    <?php include("HTML/navbar.html"); ?>
     <header>
         <div class="d-flex justify-content-center py-2">
             <p class="display-4">Centro de Ayuda</p>
@@ -24,36 +26,37 @@
     </header>
     <main id="contenedor-preguntas" class="mx-auto my-4 accordion">
         <section class="pregunta accordion-item">
-            <h2 id="encabezado-pregunta-1" class="encabezado-pregunta accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta1" aria-controls="respuesta1">Pregunta 1</button></h2>
+            <h2 id="encabezado-pregunta-1" class="encabezado-pregunta accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta1" aria-controls="respuesta1">¿Qué método es mejor para dejarlo, poco a poco o de golpe?</button>
+            </h2>
             <div class="accordion-collapse collapse respuesta" id="respuesta1" aria-labelled="encabezado-pregunta-1" data-bs-parent="#contenedor-preguntas">
-                <div class="accordion-body">Respuesta 1</div>
+                <div class="accordion-body">Cualquier método vale. Lo importante es conseguir dejarlo. De todas formas, lo adecuado es adecuar el método al tipo y personalidad del fumador y pactarlo con el médico o enfermera. Si se elige el método lento hay que advertir del peligro de quedarse en fumar sólo tres o cuatro y advertir que luego es más difícil llegar a la abstinencia total.</div>
             </div>
         </section>
         <section class="pregunta accordion-item">
-            <h2 id="encabezado-pregunta-2" class="encabezado-pregunta accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta2" aria-controls="respuesta2">Pregunta 2</button></h2>
+            <h2 id="encabezado-pregunta-2" class="encabezado-pregunta accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta2" aria-controls="respuesta2">¿Es más peligroso fumar una caja que fumar tres o cuatro cigarrillos al dia?</button>
+            </h2>
             <div class="accordion-collapse collapse respuesta" id="respuesta2" aria-labelled="encabezado-pregunta-2" data-bs-parent="#contenedor-preguntas">
-                <div class="accordion-body">Respuesta 2</div>
+                <div class="accordion-body">Es más peligroso fumar una caja, pero tres o cuatro al día también matan. De todas formas es muy probable que, por el tremendo poder adictivo de la nicotina, se vaya incrementando la cifra y un día se llegue a la cajetilla.</div>
             </div>
         </section>
         <section class="pregunta accordion-item">
-            <h2 id="encabezado-pregunta-3" class="encabezado-pregunta accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta3" aria-controls="respuesta3">Pregunta 3</button></h2>
+            <h2 id="encabezado-pregunta-3" class="encabezado-pregunta accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta3" aria-controls="respuesta3">¿Qué se puede hacer para evitar las recaídas?</button>
+            </h2>
             <div class="accordion-collapse collapse respuesta" id="respuesta3" aria-labelled="encabezado-pregunta-3" data-bs-parent="#contenedor-preguntas">
-                <div class="accordion-body">Respuesta 3</div>
+                <div class="accordion-body">Primero, no angustiarse por las recaídas. Forman parte del proceso de deshabituación. Lo mejor es prevenirlas. Tener claro las situaciones en las que es más fácil recaer. Prevenirlas y tener preparada una estrategia. Una de ellas puede ser salir de la situación, abandonar el lugar. Muy útil es el retrasar el encender el cigarro interponiendo una tarea. Lo más probable es que cuando se finalice ésta, haya desaparecido el deseo, o la menos disminuido tanto que resulte tolerable.</div>
             </div>
         </section>
         <section class="pregunta accordion-item">
-            <h2 id="encabezado-pregunta-4" class="encabezado-pregunta accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta4" aria-controls="respuesta4">Pregunta 4</button></h2>
+            <h2 id="encabezado-pregunta-4" class="encabezado-pregunta accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta4" aria-controls="respuesta4">Si lo dejo, ¿cuánto tiempo tardan mis pulmones en recuperar?</button></h2>
             <div class="accordion-collapse collapse respuesta" id="respuesta4" aria-labelled="encabezado-pregunta-4" data-bs-parent="#contenedor-preguntas">
-                <div class="accordion-body">Respuesta 4</div>
-            </div>
-        </section>
-        <section class="pregunta accordion-item">
-            <h2 id="encabezado-pregunta-5" class="encabezado-pregunta accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#respuesta5" aria-controls="respuesta5">Pregunta 5</button></h2>
-            <div class="accordion-collapse collapse respuesta" id="respuesta5" aria-labelled="encabezado-pregunta-5" data-bs-parent="#contenedor-preguntas">
-                <div class="accordion-body">Respuesta 5</div>
+                <div class="accordion-body">Depende de los factores de riesgo previos, como padecer diabetes, asma u otras enfermedades. Pero, ya desde el primer día se regulariza la tensión arterial, la concentración de oxígeno en la sangre y la frecuencia cardiaca. Y a los 20 días ya mejora la circulación sanguínea y la función respiratoria.</div>
             </div>
         </section>
     </main>
-    <?php include("HTML/footer.html");?>
- </body>
+    <?php include("HTML/footer.html"); ?>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,27 +1,47 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include("HTML/global_dependencies.html"); ?>
     <link rel="stylesheet" href="CSS/pagina_principal.css">
-    <?php include("HTML/global_dependencies.html");?>
 </head>
 
-<body style="background-color: #4CBB17;">
-    <?php include("HTML/navbar.html");?>
+<body>
+    <?php include("HTML/navbar.html"); ?>
     <div>
-        <h1 class="display-4 py-3 text-light">Pagina de guianza para abandonar la adiccion al tabaco</h1>
-        <h5 class="text-light px-5 text-center">A todo aquel que este buscando abandonar el tabaco, esta pagina sirve como primer paso. A continuacion, se muestran consejos, contactos, y fuentes de informacion para empezar tu viaje.</h5>
+        <h1 class="display-4 py-3">Pagina de guianza para abandonar la adiccion al tabaco</h1>
+        <h5 class="px-5 text-center">A todo aquel que este buscando abandonar el tabaco, esta pagina sirve como primer paso. A continuacion, se muestran consejos, contactos, y fuentes de informacion para empezar tu viaje.</h5>
     </div>
-    <section  id="main-content">	
+    <section id="main-content">
         <article>
-            <header >
+            <header>
                 <h1> Pagina proyecto para ayudar a dejar de fumar </h1>
             </header>
-            <?php include("HTML/carousel.html");?>
-        </article> 	
+            <?php include("HTML/carousel.html"); ?>
+        </article>
     </section>
-
-    <?php include("HTML/footer.html");?>
+    <div class="d-flex justify-content-evenly">
+        <ul class="carta-wrapper">
+            <li class="carta">
+                <img src='Imagenes/apoyo.jpg' alt=''>
+                <h3>Busque ayuda</h3>
+                <p>No hay por que atravesar este proceso en solitario, busque apoyo de sus familiares y amigos.</p>
+            </li>
+            <li class="carta">
+                <img src='Imagenes/terapia.jpg' alt=''>
+                <h3><a href="https://medlineplus.gov/spanish/ency/article/007440.html">Centros de ayuda</a></h3>
+                <p>Hay asociaciones que organizan encuentros para que los fumadores puedan contar sus experiencias y ayudarse el uno con el otro.</p>
+            </li>
+            <li class="carta">
+                <img src='https://images.unsplash.com/photo-1613230485186-2e7e0fca1253?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHw&ixlib=rb-1.2.1&q=80&w=400' alt=''>
+                <h3>Da el primer paso</h3>
+                <p>Puedes empezar hoy mismo tu viaje hacia una vida mas sana. El viaje de mil millas comienza con un paso.</p>
+            </li>
+        </ul>
+    </div>
+    <?php include("HTML/footer.html"); ?>
 </body>
+
 </html>
